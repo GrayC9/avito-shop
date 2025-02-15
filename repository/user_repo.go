faa5313cc9d98781e9825e27f66daba92eb27465
Danila Manakov <Telegraphrabbit@yandex.ru>
2025-02-15 19:58:10 +0300

    auth test handler and utils

--- repository/user_repo.go
+++ repository/user_repo.go
@@ -1,21 +1,22 @@
 package repository
 
-import (
-	"avito-shop/config"
-	"avito-shop/models"
-)
-
-func CreateUser(name string) error {
-	user := models.User{Name: name, Coins: 1000}
-	return config.DB.Create(&user).Error
-}
-
-func GetUserByName(name string) (models.User, error) {
-	var user models.User
-	err := config.DB.Where("name = ?", name).First(&user).Error
-	return user, err
-}
-
-func UpdateUserCoins(name string, newCoins int) error {
-	return config.DB.Model(&models.User{}).Where("name = ?", name).Update("coins", newCoins).Error
-}
+//
+//import (
+//	"avito-shop/config"
+//	"avito-shop/models"
+//)
+//
+//func CreateUser(name string) error {
+//	user := models.User{Name: name, Coins: 1000}
+//	return config.DB.Create(&user).Error
+//}
+//
+//func GetUserByName(name string) (models.User, error) {
+//	var user models.User
+//	err := config.DB.Where("name = ?", name).First(&user).Error
+//	return user, err
+//}
+//
+//func UpdateUserCoins(name string, newCoins int) error {
+//	return config.DB.Model(&models.User{}).Where("name = ?", name).Update("coins", newCoins).Error
+//}
